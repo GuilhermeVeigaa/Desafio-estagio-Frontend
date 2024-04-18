183464cbb0e384f92d2a7815da252f3dc6acb8ff
GuilhermeVeigaa <guilhermeveigafff@gmail.com>
2024-04-18 02:37:51 -0300

    add Navbar

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import Search from './components/Search.vue';
+
+
+</script>
+
+<template>
+  <header>
+    <nav>
+      <img class="pokeball-logo" src="../public/pokeball.png" alt="Logo do site: uma pokébola">
+
+      <Search />
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+  nav {
+    min-height: 20vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #0000ff;
+  }
+
+  .pokeball-logo {
+    margin-top: 20px;
+    width: 84px;
+    }
+
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,74 @@
+<script setup>
+     const placeholder = "Nome, ID, Tipo..."
+
+</script>
+
+<template>
+   <h3 class="title">O que você está procurando na Pokédex ?</h3>
+
+   <div class="search-container">
+        <img class="search-logo" width="22px" src="../../public/search.png" alt="Search logo">
+        
+        <input type="search" name="search" id="search" :placeholder="placeholder">
+
+        <button class="buttton-search">Buscar</button>
+        
+    </div>
+</template>
+
+<style scoped>
+    .title{
+      color: white;
+      padding-top: 20px;
+    }
+
+    .search-container {
+        display: flex;
+        position: relative;
+    }
+
+    #search {
+        margin-top: 20px;
+        margin-bottom: 30px;
+        padding: 10px 2.5rem;
+
+        border-radius: 10px;
+        border: none;
+        height: 44px;
+        min-width: 18rem;
+
+        font-size: 15px;
+        font-weight: 700;
+    }
+
+    #search:focus {
+        outline: none;
+    }
+
+    .search-logo {
+        color: #000;
+        position: absolute;
+        padding: 11% 10px;
+    }
+
+    .buttton-search {
+        border-radius: 20px;
+        border: none;
+        width: 4.3rem;
+        height: 2.4rem;
+
+        position: absolute;
+        margin: 8% 75%;
+
+        color: #fffefe;
+        background: #000000e0;
+
+        font-weight: 700;
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif
+    }
+
+    button:hover {
+        cursor: pointer;
+        opacity: 0.8;
+    }
+</style>
